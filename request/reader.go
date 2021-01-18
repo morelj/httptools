c09e87e5210c51e1c55623d3eaabcf3fec1c5c3c
Julien Morel <julien@xo7.fr>
2021-01-18 21:07:54 +0100

    Added request body reader

---
+++ request/reader.go
@@ -0,0 +1,51 @@
+package request
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type Reader struct {
+	r *http.Request
+}
+
+func NewReader(r *http.Request) Reader {
+	return Reader{r: r}
+}
+
+func (r Reader) Bytes() ([]byte, error) {
+	defer r.r.Body.Close()
+	return ioutil.ReadAll(r.r.Body)
+}
+
+func (r Reader) MuseBytes() []byte {
+	data, err := r.Bytes()
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func (r Reader) String() (string, error) {
+	data, err := r.Bytes()
+	return string(data), err
+}
+
+func (r Reader) MustString() string {
+	return string(r.MuseBytes())
+}
+
+func (r Reader) UnmarshalJSON(v interface{}) error {
+	data, err := r.Bytes()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, v)
+}
+
+func (r Reader) MustUnmarshalJSON(v interface{}) {
+	if err := r.UnmarshalJSON(v); err != nil {
+		panic(err)
+	}
+}
